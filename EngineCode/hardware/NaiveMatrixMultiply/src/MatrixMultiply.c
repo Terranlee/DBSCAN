ca491624f03ac81fc3ccd5dc96c4b4398b0eca6b
Terranlee <terranleethu@gmail.com>
2015-07-21 09:57:39 +0100

    update SLiC interface

--- EngineCode/hardware/NaiveMatrixMultiply/src/MatrixMultiply.c
+++ EngineCode/hardware/NaiveMatrixMultiply/src/MatrixMultiply.c
@@ -9,6 +9,7 @@ const int m_num_points_cell = 10;
 
 void test_dfe(){
 	int num_cells = 1000;
+	int num_of_points = num_cells * m_num_points_cell;
 	int length = num_cells * m_num_points_cell * 2;
 	float* arr = malloc(sizeof(float) * length);
 	for(int i=0; i<length; i++)
@@ -16,7 +17,8 @@ void test_dfe(){
 
 	float answer = malloc(sizeof(float) * num_cells);
 	float eps = 0.1;
-	MatrixMultiply(num_cells, eps, arr, answer);
+	int minPts = 5;
+	MatrixMultiply(num_of_points, eps, minPts, arr, answer);
 
 }
 
