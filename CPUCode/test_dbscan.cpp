c158a098eceab2d9763571ad88e71492001fd49c
2012011320 <ltrthu@163.com>
2015-07-24 17:27:11 +0100

    change test_dbscan.cpp, use -DDESIGN_NAME to detect environment

--- CPUCode/test_dbscan.cpp
+++ CPUCode/test_dbscan.cpp
@@ -1,5 +1,12 @@
 #include "dbscan_matrix.h"
 #include "dbscan_grid.h"
+#include "dbscan_reduced.h"
+#include "dbscan_rehash.h"
+
+// include dbscan_dfe.h only when you have the maxeler environment
+#ifdef DESIGN_NAME
+    #include "dbscan_dfe.h"
+#endif
 
 #include <algorithm>
 
@@ -53,7 +60,6 @@ void test_grid(Labels& label_grid){
     delete dbs;
 }
 
-/*
 void test_reduced(Labels& label_reduced){
     
     //DBSCAN* dbs = new DBSCAN_Reduced(20000, 4);   // the papameter for s1.txt
@@ -78,30 +84,32 @@ void test_reduced(Labels& label_reduced){
     delete dbs;
 }
 
-void test_dfe(Labels& label_dfe){
+// test dbscan on dfe only when you have maxeler environment
+#ifdef DESIGN_NAME
+    void test_dfe(Labels& label_dfe){
 
-    //DBSCAN* dbs = new DBSCAN_DFE(20000, 4);   // the papameter for s1.txt
-    //dbs->read_cluster_data(2, 5000, "../data/s1.txt");
+        //DBSCAN* dbs = new DBSCAN_DFE(20000, 4);   // the papameter for s1.txt
+        //dbs->read_cluster_data(2, 5000, "../data/s1.txt");
 
-    DBSCAN* dbs = new DBSCAN_DFE(10000, 4);
-    dbs->read_cluster_data(2, 25000, "../data/5times_s1.txt");
+        DBSCAN* dbs = new DBSCAN_DFE(10000, 4);
+        dbs->read_cluster_data(2, 25000, "../data/5times_s1.txt");
     
-    cout<<"start execution of dfe DBSCAN"<<endl;
-    float begin = DBSCAN::get_clock();
-    dbs->fit();
-    float end = DBSCAN::get_clock();
-    cout<<"time is : "<<end - begin<<endl;
+        cout<<"start execution of dfe DBSCAN"<<endl;
+        float begin = DBSCAN::get_clock();
+        dbs->fit();
+        float end = DBSCAN::get_clock();
+        cout<<"time is : "<<end - begin<<endl;
 
-    dbs->reshape_labels();
-    dbs->output_result("output_dfe");
+        dbs->reshape_labels();
+        dbs->output_result("output_dfe");
 
-    Labels lbr = dbs->get_labels();
-    label_dfe.resize(lbr.size());
-    std::copy(lbr.begin(), lbr.end(), label_dfe.begin());
-    cout<<endl;
-    delete dbs;
-}
-*/
+        Labels lbr = dbs->get_labels();
+        label_dfe.resize(lbr.size());
+        std::copy(lbr.begin(), lbr.end(), label_dfe.begin());
+        cout<<endl;
+        delete dbs;
+    }
+#endif
 
 int main()
 {
