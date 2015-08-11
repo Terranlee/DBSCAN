75b7930270a229fc3d9f3dd63151d7d8b8a09713
2012011320 <ltrthu@163.com>
2015-08-11 14:27:45 +0100

    add the code to change from my data format to CUHK format

---
+++ data/to_cuhk.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+
+#using namespace std;
+
+// change the data format to CUHK SIGMOD15 DBSCAN
+void to_cuhk(const string filename, int input_size, int dimension){
+    ifstream fin(filename.data());
+    string output_file = filename + ".ds";
+    ofstream fout(output_file.data());
+
+    fout<<input_size<<"\t"<<dimension<<endl;
+    float temp;
+    for(int i=0; i<input_size; i++){
+        fout<<i;
+        for(int j=0; j<dimension; j++){
+            fin>>temp;
+            fout<<"\t"<<temp;
+        }
+        fout<<endl;
+    }
+
+    fout.close();
+    fin.close();
+}
+
+int main(){
+
+    return 0;
+}
