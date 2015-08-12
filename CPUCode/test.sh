247345dd000781d54e14296b5f85cb4f9b85720e
Terranlee <ltrthu@163.com>
2015-08-12 01:16:04 +0100

    update test.sh

--- CPUCode/test.sh
+++ CPUCode/test.sh
@@ -1,12 +1,45 @@
 #!/bin/bash 
 clear
 
-for i in `seq 3`
+for i in `seq 10`
 do
-    ./test_dbscan 10 >> result
+	echo 10
+    ./test_dbscan 10 >> output
 done
 
-for i in `seq 5`
+for i in `seq 10`
 do
-    ./test_dbscan 20 >> result
+	echo 20
+	./test_dbscan 20 >> output
 done
+
+for i in `seq 10`
+do
+	echo 30
+    ./test_dbscan 30 >> output
+done
+
+for i in `seq 10`
+do
+	echo 40
+	./test_dbscan 40 >> output
+done
+
+for i in `seq 10`
+do
+	echo 50
+	./test_dbscan 50 >> output
+done
+
+for i in `seq 10`
+do 
+	echo 70
+	./test_dbscan 70 >> output
+done
+
+for i in `seq 10`
+do
+	echo 100
+	./test_dbscan 100 >> output
+done
+
